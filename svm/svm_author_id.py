9d63c1ae12cd0519dcdc648eaefc790f6d94c846
Anup Warnulkar <anup.warnulkar@gmail.com>
2017-04-16 20:17:01 +0530

    SVM with rbf kernel - predict ith element of test_label

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,14 +26,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 from sklearn import svm
 from sklearn.metrics import accuracy_score
+
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+
 clf = svm.SVC(kernel="rbf", C=10000)
 t0 = time()
 clf.fit(features_train, labels_train)
 print "training time: ", round(time()-t0, 3), "s"
-t1 = time()
-labels_pred = clf.predict(features_test)
-print "predicting time: ", round(time()-t1, 3), "s"
-print "Accuracy: ", accuracy_score(labels_test, labels_pred)
+
+test_index = 26
+labels_pred = clf.predict(features_test[test_index])
+
+print "Prediction label: ", labels_pred
+print "Actual label: ", labels_test[test_index]
 
 #########################################################
 
