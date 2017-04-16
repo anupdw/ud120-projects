0465d55bdf43d7ed060532a970f3e208a8c11741
Anup Warnulkar <anup.warnulkar@gmail.com>
2017-04-16 14:47:53 +0530

    SVM with linear kernel
    Test SVM on email messages from Chris and Sara. Compute accuracy,
    training time and prediction time. Compare this with training and
    prediction times of Naive Bayes classifier.

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,16 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+clf = svm.SVC(kernel="linear")
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time: ", round(time()-t0, 3), "s"
+t1 = time()
+labels_pred = clf.predict(features_test)
+print "predicting time: ", round(time()-t1, 3), "s"
+print "Accuracy: ", accuracy_score(labels_test, labels_pred)
 
 #########################################################
 
