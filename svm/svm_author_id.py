9f6a22a591e6a37170dc3d130e13332476e68916
Anup Warnulkar <anup.warnulkar@gmail.com>
2017-04-16 18:43:56 +0530

    SVM with rbf kernel - increased C param but larget dataset to increase accuracy
    Pretty Good Accuracy finally!

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,8 +24,6 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 clf = svm.SVC(kernel="rbf", C=10000)
