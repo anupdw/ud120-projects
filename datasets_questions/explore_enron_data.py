5cee2314a111bec73e4602498da5b9e551c11f35
Anup Warnulkar <anup.warnulkar@gmail.com>
2017-05-07 04:24:31 +0530

    datasets questions - part1

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -16,7 +16,35 @@
 """
 
 import pickle
+from math import isnan
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+
+print "Number of People: ",len(enron_data.keys())
+print "Number of features: ", max([len(value) for key,value in enron_data.items()])
+print "Number of Persons of Interest: ", len([value["poi"] for key,value in enron_data.items() if value["poi"]==1])
+
+print "Total stock with James Prentice: ", enron_data["PRENTICE JAMES"]["restricted_stock"] + enron_data["PRENTICE JAMES"]["exercised_stock_options"]
+
+print "Number of emails from Wesley Colwell to POI: ", enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+
+print "value of stock options exercised by Jeffrey K Skilling: ", enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+
+poi_total_payments = 0
+for person in ["LAY KENNETH L", "SKILLING JEFFREY K", "FASTOW ANDREW S"] :
+    if enron_data[person]["total_payments"] > poi_total_payments:
+        poi = person
+        poi_total_payments = enron_data[person]["total_payments"] 
+print "Person with Highest payment: ", poi, " total payments: ", poi_total_payments 
+
+poi_email_address_count = 0
+poi_salary_count = 0
+for k,v in enron_data.items():
+    if v["email_address"] != 'NaN':
+        poi_email_address_count += 1
+    if isinstance(v["salary"], int):
+        poi_salary_count +=1
+print "Number of people with email address: ", poi_email_address_count  
+print "Number of people with known salary: ", poi_salary_count
 
 
